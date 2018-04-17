7d0bc5c5a44dae3bd2d28b1f0d4852ace6082179
tkc <info@onga.jp>
2018-04-17 23:02:48 +0900

    Add account

---
+++ pages/accounts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Value</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Accounts</td>
+        <td>
+          <ul>
+            <li :key="account" v-for="account in Accounts">{{account}}</li>
+          </ul>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  import web3 from '../helper/web3';
+  export default {
+    data() {
+      return {
+        Accounts: [],
+      }
+    },
+    beforeMount(){
+      web3.eth.getAccounts().then(val=>this.Accounts=val);
+    },
+  }
+</script>
