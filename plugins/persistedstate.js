2f44c5b388f3b70f920901e0b26f47bea172d433
tkc <info@onga.jp>
2018-04-18 21:04:40 +0900

    Add vue-persistedstate

---
+++ plugins/persistedstate.js
@@ -0,0 +1,14 @@
+import createPersistedState from "vuex-persistedstate";
+
+export default ({store, isHMR}) => {
+  // In case of HMR, mutation occurs before nuxReady, so previously saved state
+  // gets replaced with original state received from server. So, we've to skip HMR.
+  // Also nuxtReady event fires for HMR as well, which results multiple registration of
+  // vuex-persistedstate plugin
+  if (isHMR) return;
+  if (process.client) {
+    window.onNuxtReady((nuxt) => {
+      createPersistedState()(store); // vuex plugins can be connected to store, even after creation
+    });
+  }
+};
