cb3c54101620309230e07759313b94ed4e9218d1
tkc <info@onga.jp>
2018-04-18 21:06:35 +0900

    Update send view

--- pages/send.vue
+++ pages/send.vue
@@ -10,8 +10,12 @@
         <input type="text" class="form-control" placeholder="Balance" v-model="Balance" disabled>
       </div>
       <div class="form-group">
+        <label>PassWord</label>
+        <input type="text" class="form-control" placeholder="PassWord" v-model="PassWord">
+      </div>
+      <div class="form-group">
         <label>SendValue</label>
-        <input type="text" class="form-control" placeholder="Balance" v-model="SendValue">
+        <input type="text" class="form-control" placeholder="SendValue" v-model="SendValue">
       </div>
       <div class="form-group">
         <label>To</label>
@@ -35,6 +39,7 @@
       return {
         CoinBase:"",
         Balance: 0,
+        PassWord:"",
         SendValue:1,
         TransactionHash:"",
         Accounts: [],
@@ -59,18 +64,25 @@
             }).then(val=>this.EstimateGas=val)
       },
       send(){
-        web3.eth.sendTransaction({
-          from: this.CoinBase,
-          to: this.To,
-          value: this.SendValue
-        }, (error, txHash) => {
-          console.log("Transaction Hash:", txHash, error);
-          this.TransactionHash = txHash;
-          if(!error) {
-            web3.eth.getBalance(this.CoinBase).then(val=>this.Balance=val);
-          } else {
-            alert("Ether Transfer Failed");
-          }
+        web3.eth.personal.unlockAccount(this.CoinBase, this.PassWord).then(()=> {
+          web3.eth.sendTransaction({
+            from: this.CoinBase,
+            to: this.To,
+            value: this.SendValue
+          }, (error, txHash) => {
+            console.log("Transaction Hash:", txHash, error);
+            this.TransactionHash = txHash;
+            this.SendValue = 0;
+            this.To = this.Accounts[0];
+            if (!error) {
+              web3.eth.getBalance(this.CoinBase).then(val => this.Balance = val);
+            } else {
+              alert("Ether Transfer Failed");
+            }
+
+          });
+        }).catch(()=>{
+          alert("can't lock account");
         });
       }
     },
